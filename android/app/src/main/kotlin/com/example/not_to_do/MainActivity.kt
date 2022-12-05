653bd41474e571d77da1c8928e9284d8bf06d50b
Eastyn <eastynwork@gmail.com>
2022-12-05 22:31:43 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/not_to_do/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.not_to_do
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber[50],
+        appBar: AppBar(
+          title: const Text('Not To Do App'),
+          backgroundColor: Color(0x919D0161),
+        ),
+        body: Center(),
+      ),
+    ),
+  );
+}
