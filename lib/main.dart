1aa6f9609994f40cbfa0d39144bed33511c63cbb
Eastyn <eastynwork@gmail.com>
2022-12-06 22:46:54 +0900

    Add a Checkbox example to learn the Checkbox Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,65 @@
-// import 'dart:collection';
+import 'dart:collection';
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = "Not To Do";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.amber[50],
         appBar: AppBar(
-          title: const Text('Not To Do App'),
-          backgroundColor: Color(0x919D0161),
+          title: const Text(_title),
+          backgroundColor: const Color(0x919D0161),
         ),
-        body: Center(),
+        body: const Center(
+          child: MyStatefulWidget(),
+        ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidget();
+}
+
+class _MyStatefulWidget extends State<MyStatefulWidget> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
+    return Checkbox(
+        checkColor: Colors.blue[50],
+        fillColor: MaterialStateProperty.resolveWith(getColor),
+        value: isChecked,
+        onChanged: (bool? value) {
+          setState(() {
+            isChecked = value!;
+          });
+        });
+  }
 }
